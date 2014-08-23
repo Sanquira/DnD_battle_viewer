f88ed2c0f940bb1cf0e0d64c37ddb14c65763ee4
Sanquira <sanquira.van.delbar@gmail.com>
2014-08-23 15:13:17 +0200

    Oprava Hex.java

--- src/hexapaper/Hex.java
+++ src/hexapaper/Hex.java
@@ -10,6 +10,7 @@ import hexapaper.source.Sklad.PropPair;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
@@ -23,7 +24,6 @@ import javax.swing.ScrollPaneConstants;
 public class Hex {
 
 	private JFrame frame;
-
 
 	/**
 	 * Launch the application.
@@ -48,7 +48,6 @@ public class Hex {
 		initialize();
 	}
 
-
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -57,149 +56,72 @@ public class Hex {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
-		final Properties p=new Properties();
+
+		final Properties p = new Properties();
 		p.setBounds(202, 0, 201, 261);
 		frame.getContentPane().add(p);
-		MouseListener al=new MouseListener(){
-
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
+		MouseListener al = new MouseAdapter() {
 
 			@Override
 			public void mousePressed(MouseEvent e) {
 				p.update((Artefact) e.getSource());
-				
+
 			}
 
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
 		};
-		MouseListener al2=new MouseListener(){
-
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
+		MouseListener al2 = new MouseAdapter() {
 
 			@Override
 			public void mousePressed(MouseEvent e) {
 				p.update((Postava) e.getSource());
-				
+
 			}
 
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
 		};
 		JLabel lblNewLabel = new JLabel("New label");
 		lblNewLabel.setBounds(27, 10, 89, 14);
 		frame.getContentPane().add(lblNewLabel);
-		
-		ArrayList<PropPair> t= new ArrayList<>();
-		t.add(new PropPair("Rasa","Elf"));
-		t.add(new PropPair("Health","8"));
-		t.add(new PropPair("Magy","15"));
-		t.add(new PropPair("Weapon","Big huge Weapon"));
-		t.add(new PropPair("Zbroj","Plate Armor"));
-		t.add(new PropPair("Health","8"));
-		final Postava btn = new Postava("Sprt",new Location(5,4, 0),false,t);
+
+		ArrayList<PropPair> t = new ArrayList<>();
+		t.add(new PropPair("Rasa", "Elf"));
+		t.add(new PropPair("Health", "8"));
+		t.add(new PropPair("Magy", "15"));
+		t.add(new PropPair("Weapon", "Big huge Weapon"));
+		t.add(new PropPair("Zbroj", "Plate Armor"));
+		t.add(new PropPair("Health", "8"));
+		final Postava btn = new Postava("Sprt", new Location(5, 4, 0), false, t);
 		btn.addMouseListener(al2);
 		btn.setBounds(27, 35, 89, 23);
 		frame.getContentPane().add(btn);
-		
-		final Postava crh = new Postava("Deserd",new Location(5,4, 0), true,t);
+
+		final Postava crh = new Postava("Deserd", new Location(5, 4, 0), true, null);
 		crh.addMouseListener(al2);
-		crh.addParam("sexy","ano");
+		crh.addParam("sexy", "ano");
 		crh.setBounds(27, 62, 89, 23);
 		frame.getContentPane().add(crh);
-		
-		ArrayList<PropPair> x=new ArrayList<>();
-		x.add(new PropPair("sexy","ano"));		
-		final Artefact a = new Artefact("Object", new Location(5,4, 0), x);
+
+		ArrayList<PropPair> x = new ArrayList<>();
+		x.add(new PropPair("sexy", "ano"));
+		final Artefact a = new Artefact("Object", new Location(5, 4, 0), x);
 		a.addMouseListener(al);
 		a.setBounds(27, 104, 89, 23);
 		frame.getContentPane().add(a);
-		
 
 		JButton newchar = new JButton("New button");
 		newchar.setBounds(65, 238, 89, 23);
-		newchar.addMouseListener(new MouseListener(){
-
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseExited(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
+		newchar.addMouseListener(new MouseAdapter() {
 
 			@Override
 			public void mousePressed(MouseEvent arg0) {
-				List<Postava> t=new ArrayList<>();
+				List<Postava> t = new ArrayList<>();
 				t.add(crh);
 				t.add(btn);
-				Postava[] p={};
-				SaveFile x=new SaveFile(t);	
-				
+				Postava[] p = {};
+				SaveFile x = new SaveFile(t);
+
 			}
 
-			@Override
-			public void mouseReleased(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
 		});
 		frame.getContentPane().add(newchar);
-		
-		
-
 	}
 }
-
